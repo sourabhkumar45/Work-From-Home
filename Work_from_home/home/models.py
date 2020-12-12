999c9b9c2f29aa049d014073469afa20d45ef01b
Sourabh Kumar <55038876+sourabhkumar45@users.noreply.github.com>
2020-12-12 15:18:09 +0530

    sorted

--- Work_from_home/home/models.py
+++ Work_from_home/home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class user(models.Model):
+    name = models.CharField(max_length=122)
+    email = models.CharField(max_length = 122)
+    phone = models.IntegerField(max_length = 12, null=True)
+    desc  = models.TextField()
+    date = models.DateField()
+    
+    def __str__(self):
+        return self.name
--- Work_from_home/home/views.py
+++ Work_from_home/home/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    # return HttpResponse("this is home page")
+    obj = user.objects.get(id=2)
     context = {
-        'variable':'this is sent',
+        'name': obj.name
     }
-    return render(request, 'index.html', context)
+    return render(request, 'index.html',context)
     
