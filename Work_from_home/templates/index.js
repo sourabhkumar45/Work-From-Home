55de65188adb949061ea07b85092378a5049480d
Sourabh Kumar <55038876+sourabhkumar45@users.noreply.github.com>
2020-12-13 00:54:39 +0530

    added timer and modal

--- Work_from_home/templates/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const start_button = document.querySelector('.header_timer');
-const time = document.querySelector('.time').value;
-console.log(time)
-start_button.addEventListener('click', function(){
-
-
-})
